c9664bb0ca94958f454fc4f234067dae51ff394b
jsvk <jsvk91+github@gmail.com>
2013-11-21 21:34:22 -0500

    Initial commit

---
+++ blackbox/bbversion.h
@@ -0,0 +1,19 @@
+//static char *serial_version = "Custom Build";
+
+#ifndef BBVERSION_H
+#define BBVERSION_H
+
+#define BBLEAN_VERSION "0.1"
+#define BBLEAN_NUMVERSION 1
+
+#ifndef BBAPPVERSION
+#define BBAPPVERSION 0.1
+#endif
+
+#define BBLEAN_RELDATE "10/2012"
+
+#ifndef BBAPPNAME
+#define BBAPPNAME "br"
+#endif
+
+#endif
